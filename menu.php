5e28da24287f41c5513b5cc1e7219bdffe0719d9
Yashpatel1522 <124055632+Yashpatel1522@users.noreply.github.com>
2023-07-25 07:30:44 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,22 +4,22 @@
 </head>
 <body>
         <ul>
-           <li><a href="Aboutus">About us</a></li>
+           <li><a href="aboutus.php">About us</a></li>
            <?php 
                 if(isset($_SESSION["unm"]))
                 {
                   echo "<li><a href='language.php'>Language</a></li>";
-                  echo "<li><a href='Logout'>Logout</a></li>";
+                  echo "<li><a href='logout.php'>Logout</a></li>";
                 }
                 else
                 {
-                  echo "<li><a href='Login'>Login</a></li>";
+                  echo "<li><a href='login.php'>Login</a></li>";
                 }
            ?>
            
-           <li><a href="Contact1">Contact us</a></li>
+           <li><a href="contact1.php">Contact us</a></li>
            <li><a href="ranking.php">Ranking</a></li>          
-           <li><a href="home">Home</a></li>
+           <li><a href="home.php">Home</a></li>
      </ul>
  
 </body>
